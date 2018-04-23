8cadf35498bba2c955f698242183359d68e43771
Jared Carey <itspandorux@gmail.com>
2018-04-24 08:52:53 +1000

    Added Lose Condition, FPS Camera can be Disabled

--- jamstudios-cycle02/Assets/Scripts/Wander.cs
+++ jamstudios-cycle02/Assets/Scripts/Wander.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class Wander : MonoBehaviour {
