0a409b03152892a3c5ca29b320320b7e6e2a1715
Jared Carey <itspandorux@gmail.com>
2018-04-24 09:35:15 +1000

    Fix Wander Hoard Issue
    - Vector Condition was 1 <= Remaining Distance, not >=

--- jamstudios-cycle02/Assets/Scripts/Wander.cs
+++ jamstudios-cycle02/Assets/Scripts/Wander.cs
@@ -18,18 +18,29 @@ public class Wander : MonoBehaviour {
 
     void Update()
     {
-        if(1 <= Vector3.Distance(agent.destination, transform.position)) {
-            agent.SetDestination(GetNewTargetLoc(10));
+
+        Debug.Log("Dist: " + Vector3.Distance(agent.destination, transform.position) +
+            "\nDest: " + agent.destination + "\t, Cur: " + transform.position);
+
+        if(1 >= Vector3.Distance(agent.destination, transform.position)) {
+            agent.SetDestination(GetNewTargetLoc(100));
+            Debug.DrawRay(agent.destination, Vector3.up);
             //Debug.Log(agent.destination);
         }
         
     }
 
     Vector3 GetNewTargetLoc(float moveRadius) {
-        Vector3 ranDir = Random.insideUnitSphere * moveRadius;
+        Vector3 ranPoint = transform.position + Random.insideUnitSphere * moveRadius;
         NavMeshHit hit;
-        NavMesh.SamplePosition(ranDir, out hit, moveRadius, NavMesh.AllAreas);
+        NavMesh.SamplePosition(ranPoint, out hit, moveRadius, NavMesh.AllAreas);
         return hit.position;
     }
 
+    Vector3 GetNewTargetLoc()
+    {
+        NavMeshTriangulation tri = NavMesh.CalculateTriangulation();
+        int t = Random.Range(0, tri.vertices.Length - 3);
+        return tri.vertices[t];
+    }
 }
