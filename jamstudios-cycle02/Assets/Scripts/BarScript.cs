1d516ec4f8c57cf8a110730ea9784f382426bac2
Unknown <melikehatcliff@hotmail.com>
2018-04-28 11:47:28 +1000

    UpdatedHudBar minus changing pics

---
+++ jamstudios-cycle02/Assets/Scripts/BarScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour {
+
+    [SerializeField]
+    private float fillAmount;
+
+    [SerializeField]
+    private Image content;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandleBar();
+	}
+
+    private void HandleBar()
+    {
+        content.fillAmount = Map(GameController.instance.chickensAlive,0,GameController.instance.loseThreshold,0,1);
+    }
+
+    private float Map(float chickensAlive, float chickenMin, float chickenMax, float editorMin ,float editorMax)
+    {
+        return (chickensAlive - chickenMin) * (editorMax - editorMin) / (chickenMax - chickenMin) + editorMin;
+    }
+}
